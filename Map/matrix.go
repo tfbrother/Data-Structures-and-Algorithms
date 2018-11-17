f88954d0e9eea01dcc2d68f0bb15181a0284db89
tfbrother <tfbrother@qq.com>
2018-11-17 23:33:13 +0800

    实现图的普拉姆最小生成树算法

--- Map/matrix.go
+++ Map/matrix.go
@@ -14,12 +14,21 @@ func NewNode(value string) Node {
 	return Node{value: value}
 }
 
+// 边(用于最小生成数使用)
+type Edge struct {
+	nodeIndexA int  // 顶点A
+	nodeIndexB int  // 顶点B
+	weight     int  // 权重
+	isSelected bool // 是否选择
+}
+
 //采用邻接矩阵存储map
 type Gmap struct {
 	nodes     []Node // 数组存储结点列表
 	matrix    []int  // 数组存储边数据矩阵
 	capacity  int    // 图的容量
 	nodeCount int    // 已添加顶点的个数
+	edges     []Edge // 保存最小生成数的边
 }
 
 // 添加顶点
@@ -136,6 +145,68 @@ func (g *Gmap) breadthFirstTraverseImpl(nodeIndexs []int) {
 	g.breadthFirstTraverseImpl(curIndexs)
 }
 
+// 普里姆最小生成数算法
+func (g *Gmap) PrimTree(startIndex int) {
+	// 获取结点所有的边，找到最小的边，然后以这条边的另一个顶点为基础找最小的边，依次下去，直到找到的边数=顶点数-1就完毕。
+	var (
+		nodeIndex    int
+		edges        []Edge // 待选边集合
+		edge         Edge
+		minEdgeIndex int
+	)
+	nodeIndex = startIndex
+	edges = make([]Edge, 0, 10)
+	g.nodes[nodeIndex].isAccess = true
+
+	for {
+		if len(g.edges) == g.capacity-1 {
+			break
+		}
+		for i := 0; i < g.capacity; i++ {
+			if g.matrix[i*g.capacity+nodeIndex] != 0 && g.nodes[i].isAccess != true { // 找到一个满足条件第一个边，放入集合，先后继续找
+				edges = append(edges, Edge{nodeIndex, i, g.matrix[i*g.capacity+nodeIndex], false})
+			}
+		}
+
+		//fmt.Println(edges)
+		// 返回一条最小的边索引
+		minEdgeIndex = g.getMinEdge(edges)
+		edge = edges[minEdgeIndex]
+		fmt.Println(edge.nodeIndexA, "====", edge.nodeIndexB, "(", edge.weight, ")")
+		edges[minEdgeIndex].isSelected = true
+		g.edges = append(g.edges, edges[minEdgeIndex])
+		//fmt.Println(edges)
+		//fmt.Println(g.nodes)
+		nodeIndex = edge.nodeIndexB
+		g.nodes[nodeIndex].isAccess = true
+	}
+}
+
+// 获取最小的边
+func (g *Gmap) getMinEdge(edges []Edge) (index int) {
+	var edge, minEdge Edge
+
+	for i := 0; i < len(edges); i++ {
+		edge = edges[i]
+		// TODO(tfbrother)避免形成闭环图，尤其是is.Access的检查，此处花了1个小时调试才找到问题
+		if edge.isSelected == true || g.nodes[edge.nodeIndexB].isAccess == true {
+			continue
+		}
+
+		if minEdge.weight == 0 {
+			minEdge = edge
+			index = i
+		} else if minEdge.weight > edge.weight {
+			minEdge = edge
+			index = i
+		}
+	}
+
+	//fmt.Println(minEdge, index)
+
+	return
+}
+
 // 初始化一个gmap
 func NewGmap(capacity int) *Gmap {
 	return &Gmap{
@@ -143,5 +214,6 @@ func NewGmap(capacity int) *Gmap {
 		matrix:    make([]int, capacity*capacity),
 		capacity:  capacity,
 		nodeCount: 0,
+		edges:     make([]Edge, 0, capacity-1),
 	}
 }
