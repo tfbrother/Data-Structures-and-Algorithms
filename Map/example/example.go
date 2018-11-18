4714c328afbceb1400d6aceec5779deb981c019a
tfbrother <tfbrother@qq.com>
2018-11-18 10:18:28 +0800

    补充图的图拉姆最小生成树算法用例

--- Map/example/example.go
+++ Map/example/example.go
@@ -6,20 +6,18 @@ import (
 )
 
 var (
-	node1, node2, node3, node4, node5, node6, node7, node8 Map.Node
-	gmap                                                   *Map.Gmap
+	node1, node2, node3, node4, node5, node6 Map.Node
+	gmap                                     *Map.Gmap
 )
 
 func main() {
-	gmap = Map.NewGmap(8)
+	gmap = Map.NewGmap(6)
 	node1 = Map.NewNode("A")
 	node2 = Map.NewNode("B")
 	node3 = Map.NewNode("C")
 	node4 = Map.NewNode("D")
 	node5 = Map.NewNode("E")
 	node6 = Map.NewNode("F")
-	node7 = Map.NewNode("G")
-	node8 = Map.NewNode("H")
 
 	gmap.AddNode(node1)
 	gmap.AddNode(node2)
@@ -27,20 +25,19 @@ func main() {
 	gmap.AddNode(node4)
 	gmap.AddNode(node5)
 	gmap.AddNode(node6)
-	gmap.AddNode(node7)
-	gmap.AddNode(node8)
 
 	//gmap.Dump()
 
-	gmap.SetUnGraphValue(0, 1, 1)
-	gmap.SetUnGraphValue(0, 3, 2)
-	gmap.SetUnGraphValue(1, 3, 3)
-	gmap.SetUnGraphValue(1, 5, 4)
-	gmap.SetUnGraphValue(2, 4, 7)
-	gmap.SetUnGraphValue(5, 4, 8)
-	gmap.SetUnGraphValue(3, 6, 3)
-	gmap.SetUnGraphValue(3, 7, 5)
-	gmap.SetUnGraphValue(6, 7, 3)
+	gmap.SetUnGraphValue(0, 1, 6)
+	gmap.SetUnGraphValue(0, 4, 5)
+	gmap.SetUnGraphValue(0, 5, 1)
+	gmap.SetUnGraphValue(1, 2, 3)
+	gmap.SetUnGraphValue(1, 5, 2)
+	gmap.SetUnGraphValue(2, 5, 8)
+	gmap.SetUnGraphValue(2, 3, 7)
+	gmap.SetUnGraphValue(3, 4, 2)
+	gmap.SetUnGraphValue(3, 5, 4)
+	gmap.SetUnGraphValue(4, 5, 9)
 
 	gmap.Dump()
 	fmt.Println("深度优先")
