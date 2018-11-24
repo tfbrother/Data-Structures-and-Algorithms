fac84d58f1db7f4b6e7905c4fb794cc8cc4edf8b
tfbrother <tfbrother@qq.com>
2018-11-24 15:21:55 +0800

    优化快速排序 解决针对近似排序好的数组，快速排序退化成近似O(n*n)复杂度的情况

--- algorithms/sorting/quick.go
+++ algorithms/sorting/quick.go
@@ -1,5 +1,10 @@
 package sorting
 
+import (
+	"math/rand"
+	"time"
+)
+
 // 快速排序
 
 func QuickSort(arr []int) {
@@ -20,6 +25,37 @@ func partition(arr []int) (i int) {
 	val := arr[0]
 	for k := 1; k < len(arr); k++ {
 		if arr[k] < val {
+			arr[i+1], arr[k] = arr[k], arr[i+1]
+			i++
+		}
+	}
+	arr[0], arr[i] = arr[i], arr[0]
+
+	return i
+}
+
+// 优化版吧
+func QuickSort1(arr []int) {
+	if len(arr) <= 1 {
+		return
+	}
+
+	p := partition1(arr)
+	QuickSort1(arr[0:p])
+	QuickSort1(arr[p+1:])
+
+	return
+}
+
+// 主要针对的优化场景是，arr基本是已经排序的情况下，退化成链表排序了。
+// 比如arr[0]已经是数组中最小的元素了，则分区每次就只能减少一个元素，近似退化成链表了，所以改成随机取一个索引来比较。
+func partition1(arr []int) (i int) {
+	rand.Seed(time.Now().UnixNano())
+	r := rand.Int() % len(arr)
+
+	arr[0], arr[r] = arr[r], arr[0]
+	for k := 1; k < len(arr); k++ {
+		if arr[k] < arr[0] {
 			arr[i+1], arr[k] = arr[k], arr[i+1]
 			i++
 		}
