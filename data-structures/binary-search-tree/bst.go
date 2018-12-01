95f0bdc66f19cfed584dc76047217e9def7cfd65
tfbrother <tfbrother@qq.com>
2018-12-01 20:19:48 +0800

    【二叉搜索树】获取最大值、最小值，删除最大值、最小值

--- data-structures/binary-search-tree/bst.go
+++ data-structures/binary-search-tree/bst.go
@@ -62,6 +62,71 @@ func (b *bst) prevOrder(node *Node) {
 	b.prevOrder(node.Right)
 }
 
+// 二叉搜索树的层序遍历（广度优先遍历）
+// 需要借助队列来实现
+func (b *bst) LevelOrder() {
+
+}
+
+// 获取最小值，根据二叉搜索树的定义，最左边的左结点就是这个最小值
+func (b *bst) MinNum() int {
+	return b.minNum(b.root)
+}
+
+// 获取以node为根的二叉搜索树的最小值
+func (b *bst) minNum(node *Node) int {
+	if node.Left == nil {
+		return node.Value
+	}
+	return b.minNum(node.Left)
+}
+
+// 获取最大值，根据二叉搜索树的定义，最右边的右结点就是这个最大值
+func (b *bst) MaxNum() int {
+	return b.maxNum(b.root)
+}
+
+// 获取以node为根的二叉搜索树的最大值
+func (b *bst) maxNum(node *Node) int {
+	if node.Right == nil {
+		return node.Value
+	}
+
+	return b.maxNum(node.Right)
+}
+
+// 删除二叉树的最小值
+func (b *bst) RemoveMin() {
+	b.root = b.removeMin(b.root)
+}
+
+// 删除以node为根的二叉搜索树，返回的是新树的根结点
+func (b *bst) removeMin(node *Node) *Node {
+	//如果最小值是叶子结点，就直接删除即可
+	//如果最小值是非叶子结点（只有右子树），则删除后，把右子树作为移动到该元素的位置即可
+	if node.Left == nil { //nil可以看成一个二叉搜索树的根结点，所以不用去检测右子树是否为nil
+		return node.Right
+	}
+
+	node.Left = b.removeMin(node.Left)
+	return node
+}
+
+// 删除二叉树的最大值
+func (b *bst) RemoveMax() {
+	b.root = b.removeMax(b.root)
+}
+
+// 删除以node为根的二叉搜索树，返回的是新树的根结点
+func (b *bst) removeMax(node *Node) *Node {
+	if node.Right == nil {
+		return node.Left
+	}
+
+	node.Right = b.removeMax(node.Right)
+	return node
+}
+
 func (b *bst) Empty() bool {
 	return b.size == 0
 }
--- data-structures/binary-search-tree/example/example.go
+++ data-structures/binary-search-tree/example/example.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	bst := binary_search_tree.NewBst(&binary_search_tree.Node{100, nil, nil})
-	fmt.Println(bst.GetSize())
+	//fmt.Println(bst.GetSize())
 
 	bst.Add(&binary_search_tree.Node{200, nil, nil})
 	bst.Add(&binary_search_tree.Node{90, nil, nil})
@@ -16,5 +16,19 @@ func main() {
 	bst.Add(&binary_search_tree.Node{89, nil, nil})
 	bst.Add(&binary_search_tree.Node{101, nil, nil})
 	bst.Add(&binary_search_tree.Node{299, nil, nil})
+	bst.PrevOrder()
+
+	minNum := bst.MinNum()
+	fmt.Println("二叉搜索树的最小值为：", minNum)
+
+	maxNum := bst.MaxNum()
+	fmt.Println("二叉搜索树的最大值为：", maxNum)
+
+	bst.RemoveMin()
+	fmt.Println("删除最小值的，前序遍历")
+	bst.PrevOrder()
+
+	bst.RemoveMax()
+	fmt.Println("删除最大值的，前序遍历")
 	bst.PrevOrder()
 }
