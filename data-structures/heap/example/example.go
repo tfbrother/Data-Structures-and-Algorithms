69a79fdddb165934634d64741e004eb1e99a3614
tfbrother <tfbrother@qq.com>
2018-12-10 14:03:16 +0800

    【堆】重构，面向接口编程

--- data-structures/heap/example/example.go
+++ data-structures/heap/example/example.go
@@ -3,27 +3,73 @@ package main
 import (
 	"fmt"
 	"github.com/tfbrother/Data-Structures-and-Algorithms/data-structures/heap"
-	"github.com/tfbrother/Data-Structures-and-Algorithms/util"
+	"math/rand"
+	"strconv"
+	"time"
 )
 
-var (
-	myarr, myarr1 []int
-)
+type Element struct {
+	key   int
+	value string
+}
+
+func (e *Element) Less(item heap.Item) bool {
+	return e.key < item.(*Element).key
+}
+
+func (e *Element) Swap(item heap.Item) {
+	e.key, item.(*Element).key = item.(*Element).key, e.key
+	e.value, item.(*Element).value = item.(*Element).value, e.value
+}
+
+func (e Element) ToString() string {
+	return "key=" + strconv.Itoa(e.key) + ",value=" + e.value
+}
 
 func main() {
 	// 堆
-	myarr = util.GenrateRandomArray(30, 10, 100)
-	fmt.Println(myarr)
+	myarr := GenrateRandomElements(30, 10, 100)
+	//fmt.Println(myarr)
 	myHeap := heap.NewMaxHeap(30)
 	for _, v := range myarr {
-		myHeap.Insert(v)
+		myHeap.Push(v)
 	}
-	myHeap.Dump()
-	myHeap.Delete()
-	myHeap.Dump()
+	//myHeap.Dump()
+	for !myHeap.Empty() {
+		item, err := myHeap.Pop()
+		if err != nil {
+			panic(err)
+		}
 
-	myarr1 = []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	fmt.Println(myarr1)
+		fmt.Print(item.(*Element).key, ":", item.(*Element).value, " ")
+	}
+
+	fmt.Println()
+
+	myarr1 := GenrateRandomElements(30, 10, 100)
 	myHeap.Init(myarr1)
-	myHeap.Dump()
+	for !myHeap.Empty() {
+		item, err := myHeap.Pop()
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Print(item.(*Element).key, ":", item.(*Element).value, " ")
+	}
+
+	fmt.Println()
+}
+
+// 生成随机数数组
+// TODO 注意返回值必须是[]heap.Item类型，而不能是[]*Element类型。
+func GenrateRandomElements(num int, start int, end int) (ret []heap.Item) {
+	ret = make([]heap.Item, num)
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < num; i++ {
+		key := rand.Int()%(end-start+1) + start
+		value := "val" + strconv.Itoa(key)
+		ret[i] = &Element{key: key, value: value}
+	}
+
+	return
 }
--- data-structures/heap/heap.go
+++ data-structures/heap/heap.go
@@ -9,8 +9,14 @@ import (
 // 最大堆：堆中任何一个结点都不大于其父结点，堆总是一个完全二叉树。
 // 从数组第一个位置开始存储结点
 
+type Item interface {
+	Less(a Item) bool
+	Swap(a Item)
+	ToString() string
+}
+
 type maxHeap struct {
-	data     []int
+	data     []Item
 	capacity int
 	count    int
 }
@@ -23,73 +29,76 @@ func (m *maxHeap) Empty() bool {
 	return m.count == 0
 }
 
-func (m *maxHeap) Insert(item int) error {
+func (m *maxHeap) Push(item Item) error {
 	if m.count >= m.capacity {
 		return errors.New("heap is full")
 	}
 	m.data[m.count+1] = item
-	m.shiftUp(m.count + 1)
+	m.siftUp(m.count + 1)
 	m.count++
 	return nil
 }
 
 // 删除的是堆的根结点，对于最小堆来说，就是最小值，对于最大堆来说就是最小值，这也就是可以利用堆排序的原理。
-func (m *maxHeap) Delete() (item int, err error) {
+func (m *maxHeap) Pop() (item Item, err error) {
 	if m.count == 0 {
-		return 0, errors.New("heap is empty")
+		return nil, errors.New("heap is empty")
 	}
 
 	item = m.data[1]
 	m.data[1] = m.data[m.count]
 	m.count--
-	m.shiftDown(1)
+	m.siftDown(1)
 
 	return item, nil
 }
 
 // 采用数组来初始化堆
-func (m *maxHeap) Init(arr []int) {
+func (m *maxHeap) Init(arr []Item) {
 	// 从第一个位置开始填充
 	copy(m.data[1:], arr)
 	m.count = len(arr)
 
 	for i := m.count / 2; i > 0; i-- {
-		m.shiftDown(i)
+		m.siftDown(i)
 	}
 }
 
-func (m *maxHeap) shiftUp(index int) {
-	for index > 1 && m.data[index] > m.data[index/2] {
-		m.data[index], m.data[index/2] = m.data[index/2], m.data[index]
+func (m *maxHeap) siftUp(index int) {
+	for index > 1 && m.data[index/2].Less(m.data[index]) {
+		m.data[index].Swap(m.data[index/2])
 		index = index / 2
 	}
 	return
 }
 
-func (m *maxHeap) shiftDown(index int) {
+func (m *maxHeap) siftDown(index int) {
 	for 2*index <= m.count {
 		j := 2 * index
-		if j+1 <= m.count && m.data[j+1] > m.data[j] {
+		if j+1 <= m.count && m.data[j].Less(m.data[j+1]) {
 			j += 1
 		}
 
-		if m.data[index] >= m.data[j] {
+		if m.data[j].Less(m.data[index]) {
 			break
 		}
 
-		m.data[index], m.data[j] = m.data[j], m.data[index]
+		m.data[index].Swap(m.data[j])
 		index = j
 	}
 }
 
 // TODO(tfbrother) 后期考虑把二叉堆的格式化输出代码实现
 func (m *maxHeap) Dump() {
-	fmt.Println(m.data[1 : m.count+1])
+	for i := 0; i < m.count; i++ {
+		fmt.Println(m.data[i+1].ToString())
+	}
+
 }
 
 func NewMaxHeap(capacity int) *maxHeap {
 	return &maxHeap{
-		data:     make([]int, capacity+1),
+		data:     make([]Item, capacity+1),
 		capacity: capacity,
 		count:    0,
 	}
