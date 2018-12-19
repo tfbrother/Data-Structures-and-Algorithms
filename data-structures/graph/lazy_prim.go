16ab1748d0752f3398a47ae6b6872a905177da24
tfbrother <tfbrother@qq.com>
2018-12-19 23:00:21 +0800

    [graph] add lazy_prim algorithms comment

--- data-structures/graph/lazy_prim.go
+++ data-structures/graph/lazy_prim.go
@@ -17,7 +17,7 @@ func (l *lazyPrim) Visit(nd Node) {
 	edges := l.g.GetNodeEdges(nd.ID())
 
 	for _, e := range edges {
-
+		// 将和节点nd相连接的所有未访问的边放入最小堆中
 		if !l.marked[e.Source().ID()] && e.Source().ID() != nd.ID() {
 			heap.Push(l.pq, e)
 		} else if !l.marked[e.Target().ID()] && e.Target().ID() != nd.ID() {
@@ -31,6 +31,8 @@ func (l *lazyPrim) LazyPrim(src ID) {
 	if err != nil {
 		return
 	}
+
+	// 算法初始化
 	for id, _ := range l.g.GetNodes() {
 		l.marked[id] = false
 	}
@@ -39,19 +41,24 @@ func (l *lazyPrim) LazyPrim(src ID) {
 	l.Visit(node)
 
 	for l.pq.Len() > 0 {
+		// 使用最小堆找出已经访问的边中权值最小的边
 		e := heap.Pop(l.pq).(Edge)
+		// 如果这条边的两端都已经访问过了, 则扔掉这条边
 		if l.marked[e.Target().ID()] == l.marked[e.Source().ID()] {
 			continue
 		}
 
+		// 否则, 这条边则应该存在在最小生成树中
 		l.mst = append(l.mst, e)
+
+		// 访问和这条边连接的还没有被访问过的节点
 		if !l.marked[e.Target().ID()] {
 			l.Visit(e.Target())
 		} else {
 			l.Visit(e.Source())
 		}
 	}
-	return
+
 	l.mstweight = l.mst[0].Weight()
 	for i := 1; i < len(l.mst); i++ {
 		l.mstweight += l.mst[i].Weight()
