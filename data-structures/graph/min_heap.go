82b12c60d8ae9626984f54586d4f3f1471e139cc
tfbrother <tfbrother@qq.com>
2018-12-19 22:56:48 +0800

    [graph] update min_heap.go

--- data-structures/graph/min_heap.go
+++ data-structures/graph/min_heap.go
@@ -9,13 +9,13 @@ func (e EdgeSlice) Less(i, j int) bool {
 	return e[i].Weight() < e[j].Weight()
 }
 func (e EdgeSlice) Swap(i, j int) { e[i], e[j] = e[j], e[i] }
-func (n *EdgeSlice) Pop() interface{} {
-	ret := (*n)[len(*n)-1]
-	*n = (*n)[:len(*n)-1]
+func (e *EdgeSlice) Pop() interface{} {
+	ret := (*e)[len(*e)-1]
+	*e = (*e)[:len(*e)-1]
 	return ret
 }
 
-func (n *EdgeSlice) Push(x interface{}) {
+func (e *EdgeSlice) Push(x interface{}) {
 	d := x.(Edge)
-	*n = append(*n, d)
+	*e = append(*e, d)
 }
