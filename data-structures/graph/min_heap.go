45c6d3d7babb2d9f139db95931bc4adf2ad6e20d
tfbrother <tfbrother@qq.com>
2018-12-20 11:21:42 +0800

    [graph] min-index heap

--- data-structures/graph/min_heap.go
+++ data-structures/graph/min_heap.go
@@ -1,5 +1,7 @@
 package graph
 
+import "container/heap"
+
 // min-heap
 // implemented heap.Interface
 type EdgeSlice []Edge
@@ -18,4 +20,48 @@ func (e *EdgeSlice) Pop() interface{} {
 func (e *EdgeSlice) Push(x interface{}) {
 	d := x.(Edge)
 	*e = append(*e, d)
+}
+
+// min-index heap
+type nodeDistance struct {
+	nd    Node
+	e     Edge
+	index int
+}
+
+type nodeDistaceHeap []nodeDistance
+
+func (d nodeDistaceHeap) Len() int { return len(d) }
+func (d nodeDistaceHeap) Less(i, j int) bool {
+	return d[i].e.Weight() < d[j].e.Weight()
+}
+
+func (d nodeDistaceHeap) Swap(i, j int) {
+	d[i], d[j] = d[j], d[i]
+	d[i].index = i
+	d[j].index = j
+}
+
+func (d *nodeDistaceHeap) Pop() interface{} {
+	heapLen := len(*d)
+	lastNode := (*d)[heapLen-1]
+	*d = (*d)[:heapLen-1]
+	lastNode.index = -1
+	return lastNode
+}
+
+func (d *nodeDistaceHeap) Push(x interface{}) {
+	nd := x.(nodeDistance)
+	nd.index = len(*d)
+	*d = append(*d, nd)
+}
+
+func (d *nodeDistaceHeap) update(nd Node, e Edge) {
+	for i := 0; i < len(*d); i++ {
+		if (*d)[i].nd.ID() == nd.ID() {
+			(*d)[i].e = e
+			heap.Fix(d, (*d)[i].index)
+			break
+		}
+	}
 }
