340c7c90420c97c4be3d5c4f73efcc3bc0218b74
tfbrother <tfbrother@qq.com>
2019-01-10 10:40:28 +0800

    [greedy algorithms] modify comment

--- algorithms/greedy/861-matrixScore.go
+++ algorithms/greedy/861-matrixScore.go
@@ -37,8 +37,8 @@ func matrixScore(A [][]int) int {
 	}
 
 	for i := 1; i < column; i++ { // 统计每列中0的数量，只要大于1的数量，就进行翻转
-		num0 := 0 // 记录每列中0的数量差
-		num1 := 0 // 记录每列中1的数量差
+		num0 := 0 // 记录每列中0的数量
+		num1 := 0 // 记录每列中1的数量
 		for j := 0; j < row; j++ {
 			if A[j][i] == 1 {
 				num1++
